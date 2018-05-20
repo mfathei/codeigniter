26b32574e683caefdabcf43c6dfde97e9d16b154
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-20 15:52:23 +0200

    Upload image using ajax

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -117,4 +117,25 @@ class Main extends CI_Controller
         $this->session->unset_userdata("username");
         redirect(base_url() . "main/login");
     }
+
+    public function image_upload()
+    {
+        $data["title"] = "Upload image using AJAX in CodeIgniter";
+        $this->load->view('image_upload', $data);
+    }
+
+    public function ajax_upload()
+    {
+        if (isset($_FILES["image_file"]["name"])) {
+            $config["upload_path"]   = "./upload";
+            $config["allowed_types"] = "jpg|jpeg|png|gif";
+            $this->load->library("upload", $config);
+            if(!$this->upload->do_upload("image_file")){
+                echo $this->upload->display_errors();
+            } else {
+                $data = $this->upload->data();
+                echo '<img src="'. base_url() .'upload/'. $data["file_name"] .'" />';
+            }
+        }
+    }
 }
---
+++ application/views/image_upload.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webslesson | <?php echo $title; ?></title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <br><br><br>
+        <h3 align="center"><?php echo $title; ?></h3>
+        <form method="post" id="upload_form" align="center" enctype="multipart/form-data">
+            <input type="file" name="image_file" id="image_file">
+            <input type="submit" name="upload" value="Upload" id="upload" calss="btn btn-info">
+        </form>
+        <br><br>
+        <div id="uploaded_image">
+            
+        </div>
+    </div>
+    <script src="/js/jquery-3.1.1.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#upload_form").on("submit", function(e){
+                e.preventDefault();
+                if($("#image_file").val() == ''){
+                    alert("Please select a file.");
+                } else {
+                    $.ajax({
+                        url: "<?php echo base_url()?>main/ajax_upload",
+                        method: "POST",
+                        data: new FormData(this),
+                        contentType: false,
+                        cache: false,
+                        processData: false,
+                        success: function(data){
+                            $("#uploaded_image").html(data);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
+</body>
+</html>
